b755507951f7dc6ccc1a0dff7c1fdac1cf923cee
Greg Guthe <gregg@aweber.com>
2012-09-09 22:16:47 -0400

    test: Expect a password for test user

--- test/add-user.sh
+++ test/add-user.sh
@@ -4,6 +4,8 @@ set registry [lindex $argv 0]
 spawn npm adduser --registry=$registry
 expect "Username:"
 send "test\n"
+expect "Password:"
+send "test\n"
 expect "Email:"
 send "test@example.com\n"
 expect eof
